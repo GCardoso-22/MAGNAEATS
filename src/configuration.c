72a2d13b0973631d7e6bb56844d58fcde3b7d589
Cardas <68824398+GCardoso-22@users.noreply.github.com>
2022-05-22 23:08:46 +0100

    big plays
    big show

--- src/configuration.c
+++ src/configuration.c
@@ -11,6 +11,7 @@ Pedro Correia - 54570
 void config(char *nomeFicheiro, struct main_data *data)
 {
     char linha[MAXLINHA];
+    char aux[8][25];
 
     FILE *file;
     file = fopen(nomeFicheiro, "r");
@@ -21,58 +22,27 @@ void config(char *nomeFicheiro, struct main_data *data)
     }
     else
     {
+
         int i = 0;
         while (fgets(linha, MAXLINHA, file) != NULL)
         {
-
-            switch(i){
-                
-                case 0:
-                    data->max_ops = atoi(linha);
-                    break;
-                case 1:
-                    data->buffers_size = atoi(linha);
-                    break;
-                case 2:
-                    data->n_clients = atoi(linha);
-                    break;
-                case 3:
-                    data->n_restaurants = atoi(linha);
-                    break;
-                case 4:
-                    data->n_drivers = atoi(linha);
-                    break;
-                case 5:
-                    printf ("linha = %s", linha);
-                    data->log_filename = linha;
-                    printf("teste + %s", data->log_filename);
-                    break;
-                
-                case 6:
-                    data->statistics_filename = linha;
-                    break;
-                case 7:
-                    data->alarm_time = atoi(linha);
-                    break;
-                default:
-                    printf("As configurações apenas esperam 8 linhas, todas as linhas posteriores serão ignoradas.\n");
-            }
+            strcpy(aux[i], linha);
             i++;
+        }        
+            
+         
+        data->max_ops = atoi(aux[0]);
+        data->buffers_size = atoi(aux[1]);
+        data->n_clients = atoi(aux[2]);
+        data->n_restaurants = atoi(aux[3]);
+        data->n_drivers = atoi(aux[4]);
+        data->log_filename = "log.txt";
+        data->statistics_filename = "statistics.txt";
+        data->alarm_time = atoi(aux[7]);
+           
+            
 
-            /**
-            strcpy(ipt[i], linha);
-            data->max_ops = atoi(ipt[0]);
-            data->buffers_size = atoi(ipt[1]);
-            data->n_clients = atoi(ipt[2]);
-            data->n_restaurants = atoi(ipt[3]);
-            data->n_drivers = atoi(ipt[4]);
-            data->log_filename = ipt[5];
-            data->statistics_filename = ipt[6];
-            data->alarm_time = atoi(ipt[7]);
-            i++;
-
-            */
-        }
+           
         fclose(file);
     }
 }
